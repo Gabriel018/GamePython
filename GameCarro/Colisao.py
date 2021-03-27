1fa2b9ee09afea68a4564b19428e571cc48fc762
Gabriel018 <duke.comandos@gmail.com>
2021-03-27 15:52:39 -0300

    colisao

---
+++ GameCarro/Colisao.py
@@ -0,0 +1,77 @@
+
+import pygame,sys
+
+def objetos():
+    global veloc_x, veloc_y,velo_z
+
+    pygame.draw.rect(tela, (255, 255, 255), Objeto1)
+    Objeto1.x += veloc_x
+    #Objeto1.y += veloc_y
+
+    pygame.draw.rect(tela, (0, 0, 0), Objeto2)
+    Objeto2.y += veloc_y
+    #coliçao com a borda Objeto 1
+    if Objeto1.right >= screm_height or Objeto1.left <=0:
+        veloc_x *= -1
+    if Objeto2.bottom >= screm_width or Objeto2.top <=0:
+        veloc_y *= -1
+
+
+    pygame.draw.rect(tela,(25,25,0),Objeto3)
+    #colisao com a borda objeto 2
+   # if Objeto2.bottom >= screm_width or Objeto2.top <=0:
+     #    veloc_y *= -1
+
+
+    #colisao com outro objeto
+
+    if Objeto1.colliderect(Objeto2):
+       veloc_x *= -1
+
+
+    """tolerance = 10
+    if Objeto1.colliderect(Objeto2):
+        if abs(Objeto2.bottom - Objeto1.top) < tolerance:
+            veloc_y *= -1
+        if abs(Objeto2.top - Objeto1.bottom) < tolerance:
+            veloc_y *= - 1
+        if abs(Objeto2.left - Objeto1.right) < tolerance:
+            veloc_x *= - 1
+        if abs(Objeto2.right- Objeto1.left) < tolerance:
+            veloc_x *= - 1"""
+pygame.init()
+
+screm_height,screm_width = 800,600
+
+clock = pygame.time.Clock()
+tela = pygame.display.set_mode((screm_height,screm_width))
+pygame.display.set_caption("Colisao")
+bg_color  = (100,100,100)
+
+
+Objeto1 = pygame.Rect(400,400,40,40)
+#velocidade
+veloc_x, veloc_y = 4, 4
+
+
+Objeto2 = pygame.Rect(750,100,40,120)
+veloc_z = 2
+
+
+Objeto3 = pygame.Rect(20,300,40,120)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+
+    tela.fill(bg_color)
+    objetos()
+    pygame.display.update()
+    clock.tick(80)
+
+
+
+
+
--- GameCarro/JogoCarro.py
+++ GameCarro/JogoCarro.py
@@ -13,19 +13,31 @@ velocidade = 10
 velocidade2 = 20
 
 fundo =  pygame.image.load("c:/JogosPython/GamePython/GameCarro/fundo.png")
-carroPreto = pygame.image.load("c:/JogosPython/GamePython/GameCarro/CarroPreto.png")
+
 carroRoxo = pygame.image.load("c:/JogosPython/GamePython/GameCarro/CarroRoxo01.png")
+
+
+carroPrincipal = pygame.image.load("c:/JogosPython/GamePython/GameCarro/CarroPreto.png")
+CarroRect = carroPrincipal.get_rect()
+
+
 carroRed = pygame.image.load("c:/JogosPython/GamePython/GameCarro/CarroRed.png")
+CarroRect2 = carroRed.get_rect()
+
+
+if CarroRect.collidepoint(100,100):
+    print("colidiu")
 
 #Cria janela
 janela = pygame.display.set_mode((800,500))
 pygame.display.set_caption("Jogo do carro")
-
 janela_aberta = True
+
+
+
 
 #Desenha Objeto
 
-colission = pygame.sprite.spritecollide(carroPreto,carroRed,True,True)
 while janela_aberta:
     pygame.time.delay(50)
     for evento in pygame.event.get():
@@ -49,9 +61,11 @@ while janela_aberta:
     pos_y += velocidade2
 
     janela.blit(fundo,(0,0))
-    janela.blit(carroPreto,(x,y))
+    janela.blit(carroPrincipal,((x,y)))
     janela.blit(carroRoxo,(pos_x +110,pos_y -400))
     janela.blit(carroRed, (pos_x, pos_y ))
+
+
     pygame.display.update()
 
 
