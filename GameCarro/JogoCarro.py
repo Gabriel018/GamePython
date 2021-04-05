904a89460ba8a885a84f05932d58d075372e8ec2
Gabriel018 <duke.comandos@gmail.com>
2021-04-05 15:55:27 -0300

    score

--- GameCarro/JogoCarro.py
+++ GameCarro/JogoCarro.py
@@ -1,25 +1,33 @@
 import pygame,sys
+from random import  randint,random
 from pygame.transform import scale
 from pygame.image import load
+from pygame.sprite import Sprite,Group,GroupSingle
+from time import sleep
 
-class jogo_car():
-
+class Cars():
 
  pygame.init()
 
-x = 400
-y = 300
-pos_x = 300
+
+x = 300
+y = 440
+pos_x = 400
 pos_y = -400
 
-velocidade = 10
+velocidade = 5
 velocidade2 = 20
+tempo = pygame.time.Clock()
+score = 0
 
+dificuldade  = 1.3
 
 
 #Cria janela
 
-janela = pygame.display.set_mode((800,600))
+altura,largura = 800,600
+
+janela = pygame.display.set_mode((altura,largura))
 fundo = pygame.image.load("c:/JogosPython/GamePython/GameCarro/estrada.png")
 fundo = pygame.transform.scale(fundo,(800,600))  #Redirecionamneto da imagem
 pygame.display.set_caption("Jogo do carro")
@@ -37,25 +45,20 @@ def Carros():
     carroRed = pygame.image.load("c:/JogosPython/GamePython/GameCarro/CarroRed.png")
     CarroRect2 = carroRed.get_rect()
     CarroRect2.x = pos_x
-    CarroRect2.y += pos_y + 100
+    CarroRect2.y += pos_y
+
+        
 
 
     carroRoxo = pygame.image.load("c:/JogosPython/GamePython/GameCarro/CarroRoxo01.png")
     CarroRect3 = carroRoxo.get_rect()
-    CarroRect3.x += pos_x + 120
+    CarroRect3.x += pos_x - 100
     CarroRect3.y = pos_y - 400
 
     carroYellow = pygame.image.load("c:/JogosPython/GamePython/GameCarro/CarroYellow.png")
     CarroRect4 = carroYellow.get_rect()
     CarroRect4.x = pos_x
     CarroRect4.y += pos_y -800
-
-
-
-
-
-
-
 
     #colisoes
     if CarroRect.colliderect(CarroRect2):
@@ -64,23 +67,17 @@ def Carros():
     if CarroRect.colliderect(CarroRect3):
         exit()
 
-
-
     #motra na tela
     janela.blit(carroPrincipal,CarroRect)
     janela.blit(carroRed,CarroRect2)
     janela.blit(carroRoxo,CarroRect3)
     janela.blit(carroYellow,CarroRect4)
 
-
-
 def controles():
-    global x, y
+    global x, y,pos_y
     comando = pygame.key.get_pressed()
     if comando[pygame.K_UP]:
-        y -= velocidade
-    if comando[pygame.K_DOWN]:
-        y += velocidade
+        pos_y += 10
     if comando[pygame.K_LEFT]:
         x -= velocidade
     if comando[pygame.K_RIGHT]:
@@ -93,16 +90,19 @@ def movinta_carro():
     pos_y += velocidade
 
 
+
 while janela_aberta:
-    pygame.time.delay(50)
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             janela_aberta = False
+
 
     janela.blit(fundo,(0,0))
     Carros()
     controles()
     movinta_carro()
+    tempo.tick(40)
     pygame.display.update()
 
 
